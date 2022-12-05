3a1a74dc8042a6d0307295a97756758388bbf1cb
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-12-05 12:53:29 +0900

    28. 不要なタグを出力しないFragmentの使い方

--- 04_react_basic/src/075_fragment/start/components/Child.js
+++ 04_react_basic/src/075_fragment/start/components/Child.js
@@ -2,9 +2,18 @@ import "./Child.css";
 
 const Child = () => {
   return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
+    <>
+      <div className="component">
+        <h3>Hello Component</h3>
+      </div>
+      <h3>Hello Fragment</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda unde
+        consequuntur libero qui placeat veritatis nihil saepe deleniti dolor
+        reiciendis iusto voluptatem, nemo eos suscipit quam eligendi iste
+        pariatur minima!
+      </p>
+    </>
   );
 };
 
